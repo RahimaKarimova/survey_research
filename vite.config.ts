117c1f16a0ce830b6f4c0b84d8ae5170bf8fed31
Rahima_karimova <118609255+RahimaKarimova@users.noreply.github.com>
2025-08-17 15:53:43 +0400

    Update vite.config.ts
    check

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // The name of your GitHub repository
 // For example, if your repository is https://github.com/your-username/my-survey-app,
 // then REPO_NAME should be 'my-survey-app'
-const REPO_NAME = 'survey';
+const REPO_NAME = 'survey_research';
 
 // https://vitejs.dev/config/
 export default defineConfig({
